12d218a02e3f25b3698a0689cf52271ef0ed0dfe
Juliano Lopes <julopys@hotmail.com>
2018-12-21 23:59:05 -0200

    Initializing a new project

---
+++ src/main/java/com/julopes/accessibleway/AccessibleWayApplication.java
@@ -0,0 +1,14 @@
+package com.julopes.accessibleway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessibleWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessibleWayApplication.class, args);
+	}
+
+}
+
